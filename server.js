ba5700977aaf2a3bdea01783e1347b0e51ec5db0
ZwarteDuivel <165676187+ZwarteDuivel@users.noreply.github.com>
2024-04-01 19:47:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/data', dataRoutes); // Use data routes
